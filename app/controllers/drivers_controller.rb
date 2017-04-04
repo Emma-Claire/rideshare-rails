95b301d02bd9fe2902cbcef19743ac6964187cd4
Tamiko Terada <tamikodevo@gmail.com>
2017-04-04 12:31:00 -0700

    add Driver#index to read in csv file

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,22 @@
+require 'csv'
+
 class DriversController < ApplicationController
+
+ def index
+  csv_text = File.read('../support/drivers.csv')
+  csv = CSV.parse(csv_text, :headers => true)
+    csv.each do |row|
+      Driver.create!(row.to_hash)
+    end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new
+    # fill out create method
+  end
+
 end
