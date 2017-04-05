a0c0a049229206c2958a79a9441816a538adeeaf
Emma Claire Philippides <acme9898@gmail.com>
2017-04-04 20:13:26 -0700

    created passenger model and controller, migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404190057) do
+ActiveRecord::Schema.define(version: 20170405030358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,24 @@ ActiveRecord::Schema.define(version: 20170404190057) do
     t.integer  "driver_id"
     t.string   "name"
     t.string   "vin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.integer  "rider_id"
+    t.string   "name"
+    t.string   "phone_num"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.integer  "trip_id"
+    t.integer  "driver_id"
+    t.integer  "rider_id"
+    t.date     "date"
+    t.integer  "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
