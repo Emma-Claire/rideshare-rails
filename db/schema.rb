ac6bae18ecf5400e2bb3c336c1166de2034bc835
Tamiko Terada <tamikodevo@gmail.com>
2017-04-06 09:04:28 -0700

    push to merge

--- db/schema.rb
+++ db/schema.rb
@@ -37,4 +37,5 @@ ActiveRecord::Schema.define(version: 20170406050647) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ csv_text = File.read(Rails.root.join('lib', 'seeds', 'drivers.csv'))
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
   driver = Driver.new
-  driver.driver_id = row['driver_id']
+  driver.id = row['driver_id']
   driver.name = row['name']
   driver.vin = row['vin']
   driver.save
@@ -24,7 +24,7 @@ passenger_data = File.read(Rails.root.join('lib', 'seeds', 'passengers.csv'))
 passenger_csv = CSV.parse(passenger_data, :headers => true)
 passenger_csv.each do |row|
   passenger = Passenger.new
-  passenger.rider_id = row['rider_id']
+  passenger.id = row['rider_id']
   passenger.name = row['name']
   passenger.phone_num = row['phone_num']
   passenger.save
@@ -34,7 +34,7 @@ trip_data = File.read(Rails.root.join('lib', 'seeds', 'trips.csv'))
 trip_csv = CSV.parse(trip_data, :headers => true)
 trip_csv.each do |row|
   trip = Trip.new
-  trip.trip_id = row['trip_id']
+  trip.id = row['trip_id']
   trip.driver_id = row['driver_id']
   trip.rider_id = row['rider_id']
   trip.date = Date.strptime(row['date'], '%Y-%m-%d')
