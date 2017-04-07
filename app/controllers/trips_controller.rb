3704a8a0306469aa3861a89fc8991ae51c4a8f6b
Emma Claire Philippides <acme9898@gmail.com>
2017-04-07 14:15:49 -0700

    fixed merge conflict

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,12 +1,6 @@
 class TripsController < ApplicationController
   def show
-<<<<<<< HEAD
-    @Trip
-  end
-
-=======
     id = params[:id].to_i
     @trip = Trip.find(id)
   end
->>>>>>> 27581488e5fe3b03b0c973af93fd06e4272586d0
 end
