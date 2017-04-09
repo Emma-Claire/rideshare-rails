fb66f8937ef1f59aacc579b83e61900e5a3ca2ae
Emma Claire Philippides <acme9898@gmail.com>
2017-04-09 16:55:38 -0700

    added validation for Trip, Driver and Rider

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,9 @@
 class Trip < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
+
+  validates :driver_id, presence: true
+  validates :passenger_id, presence: true
+  validates :date, presence: true
+  validates :rating, presence: true, length: { is: 1}
 end
